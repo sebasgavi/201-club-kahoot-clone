113c0ed9a751239acef0b0d59c9a8d2972e8e0a5
Sebastian Gaviria Giraldo <1112478378@academico.icesi.edu.co>
2020-02-20 19:08:22 -0500

    ya no se que hice aquí

--- scripts/user.js
+++ scripts/user.js
@@ -1,3 +1,12 @@
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+// var rooms = db.collection('rooms');
+// rooms.doc('35151212').set({ name: 'test' });
+
+
 var roomNumberCont = document.querySelector('.roomNumberCont');
 var userNameCont = document.querySelector('.userNameCont');
 var messageCont = document.querySelector('.messageCont');
@@ -8,8 +17,18 @@ function handleRoomNumberPanelSubmit (event) {
     var roomNumberInput = roomNumberCont.querySelector('input');
     var roomNumber = roomNumberInput.value;
 
-    roomNumberCont.classList.add('hidden');
-    userNameCont.classList.remove('hidden');
+    var room = db.collection('rooms').doc(roomNumber);
+    room.get().then(function(doc) {
+        if(doc.exists){
+            // ocultamos el panel actual
+            roomNumberCont.classList.add('hidden');
+            // mostramos el panel siguiente
+            userNameCont.classList.remove('hidden');
+        } else {
+            // mostrar error si no existe el aula
+            roomNumberCont.querySelector('.error').classList.remove('hidden');
+        }
+    });
     console.log('test', roomNumber);
 }
 roomNumberPanel.addEventListener('submit', handleRoomNumberPanelSubmit);
